67302fd2f07f6a00f90468534342633ab0a24a13
Italo Evan <igbehh@gmail.com>
2023-04-04 17:16:05 -0300

    first commit

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Meu filme";
+    }
+
+}
